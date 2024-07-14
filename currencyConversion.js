5cde5c414f7ce9deaa808d94807acf1a375eb984
ahmetSaki <ahmetsakialci@gmail.com>
2024-07-14 17:50:34 +0300

    quantity calculation added

--- currencyConversion.js
+++ currencyConversion.js
@@ -1,13 +1,17 @@
 document.getElementById('convertBtn').addEventListener('click', () => {
-    const currency = document.getElementById('moneyCode').value.trim().toUpperCase();
+    const fromCurrency = document.getElementById('fromCurrency').value.trim().toUpperCase();
+    const toCurrency = document.getElementById('toCurrency').value.trim().toUpperCase();
+    const amount = parseFloat(document.getElementById('amount').value.trim());  
 
     if (!currency) {
         document.getElementById('currency').innerHTML = '<p>Please enter a currency code.</p>';
         return;
     }
 
+
+
     const apiKey = 'fca_live_mrBxmdvK0f2GoRn09TiQt3YhofUTERF2rLVhgtm0';
-    const url = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=${currency}`;
+    const url = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=${toCurrency}&base_currency=${fromCurrency}`;
 
     fetch(url)
         .then(response => {
@@ -17,10 +21,12 @@ document.getElementById('convertBtn').addEventListener('click', () => {
             return response.json();
         })
         .then(data => {
-            if (data.data && data.data[currency]) {
-                const rate = data.data[currency];
+            if (data.data && data.data[toCurrency]) {
+                const rate = data.data[toCurrency];
+                const convertedAmount = (amount * rate).toFixed(2);
                 const currencyData = `
-                    <p>Exchange Rate (1 USD to ${currency}): ${rate}</p>`;
+                    <p>Exchange Rate (${fromCurrency} to ${toCurrency}): ${rate}</p>
+                    <p>Converted Amount: ${convertedAmount} ${toCurrency}</p>`;
                 document.getElementById('currency').innerHTML = currencyData;
             } else {
                 document.getElementById('currency').innerHTML = `<p>Invalid currency code. Please try again.</p>`;
